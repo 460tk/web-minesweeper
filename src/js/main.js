6e59d3b2e55a7a72221a8ddbf345620f59f64337
kaka_256 <kumarai460@gmail.com>
2024-08-13 13:51:05 +0900

    add L R click event

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,24 @@
+function box_open() {
+    this.classList.remove("bg-black")
+    this.classList.add("bg-gray-400")
+}
+
+function flag(){
+    this.classList.remove("bg-black")
+    this.classList.add("bg-red-400")
+}
+
 const mine_qty = []
 const game_box = document.getElementById("game_box");
 
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < 9; i++) {
     const first_box = document.createElement("div");
-    for (let j = 0; j < 3; j++) {
+    first_box.classList.add("flex")
+    for (let j = 0; j < 9; j++) {
         const second_box = document.createElement("div");
-        second_box.classList.add("w-8", "h-8", "float-left", "bg-black")
+        second_box.classList.add("w-8", "h-8", "bg-black", "m-1");
+        second_box.addEventListener("click", box_open);
+        second_box.addEventListener("contextmenu", flag);
         first_box.appendChild(second_box);
     }
     game_box.appendChild(first_box)
