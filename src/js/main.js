a7e80f65b99635a5db22fc3fd450546b8a68b0f7
kaka_256 <kumarai460@gmail.com>
2024-08-17 13:07:34 +0900

    rebuild

--- src/js/main.js
+++ src/js/main.js
@@ -1,76 +1,108 @@
 difficulty_arry = {easy:[9,9,10], nomal:[16,16,40], hard:[30,16,99]}
 
-function get_randomInt(max) {
+function get_random_int(max) {
     return Math.floor(Math.random() * max);
 }
 
-game_box = document.getElementById("game_box");
+const game_box = document.getElementById("game_box");
+const selecter = document.getElementsByClassName("difficulty");
 
 class mine_sweeper {
-    constructor(width, hight, mine) {
-        this.game_width = width;
-        this.game_hight = hight;
-        this.mine = mine;
-    }
-
-    create_map() {
-        this.mine_map = []
+    constructor(data_list) {
+        this.game_width = data_list[0];
+        this.game_hight = data_list[1];
+        this.mine = data_list[2];
+        this.first = true;
+        this.mine_map = [];
         for (let i = 0; i < this.game_hight; i++) {
             this.mine_map[i] = [];
             for (let j = 0; j < this.game_width; j++) {
                 this.mine_map[i][j] = 0;
             }
         }
+
+        this.render_map = [];
+        for (let i = 0; i < this.game_hight; i++) {
+            this.render_map[i] = [];
+            for (let j = 0; j < this.game_width; j++) {
+                this.render_map[i][j] = 0;
+            }
+        }
+    }
+    set_mine_map(no_mine) {
         let placed_mine = 0;
         while (placed_mine < this.mine) {
-            let horizon = get_randomInt(this.game_width);
-            let vertical = get_randomInt(this.game_hight);
-            if (this.mine_map[vertical][horizon] === 0) {
-                this.mine_map[vertical][horizon] = -1;
+            let random_coordinate = []
+            random_coordinate[0] = get_random_int(this.game_hight);
+            random_coordinate[1] = get_random_int(this.game_width);
+            if (this.mine_map[random_coordinate[0]][random_coordinate[1]] === 0 && JSON.stringify(random_coordinate) !== JSON.stringify(no_mine)) {
+                this.mine_map[random_coordinate[0]][random_coordinate[1]] = -1;
                 placed_mine++;
             }
         }
     }
-
-    box_open(event) {
-        let elem = event.currentTarget;
-        elem.classList.remove("bg-black");
-        elem.classList.add("bg-gray-400");
+    open_render_map(horizon, vertical) {
+        this.render_map[horizon][vertical] = 0;
     }
-    
-    flag(event) {
-        let elem = event.currentTarget;
-        elem.classList.remove("bg-black");
-        elem.classList.add("bg-red-400");
+    flag_render_map(horizon, vertical) {
+        this.render_map[horizon][vertical] = 1;
     }
-
-    game_starter() {
+    render_game_start(game) {
         while (game_box.firstChild) {
             game_box.removeChild(game_box.firstChild);
         }
-        for (let i = 0; i < this.game_hight; i++) {
+        this.html_render_list = []
+        for (let i = 0; i < this.render_map.length; i++) {
             const vertical_box = document.createElement("div");
             vertical_box.classList.add("flex");
-            for (let j = 0; j < this.game_width; j++) {
+            let horizon_html_render_list = [];
+            for (let j = 0; j < this.render_map[i].length; j++) {
                 const horizon_box = document.createElement("div");
+                horizon_box.id = `${i},${j}`;
                 horizon_box.classList.add("w-8", "h-8", "bg-green-200", "m-1", "text-center");
-                horizon_box.textContent = this.mine_map[i][j];
-                horizon_box.addEventListener("click", {obj: this, handleEvent: this.box_open});
-                horizon_box.addEventListener("contextmenu",{obj: this, handleEvent: this.flag});
+                horizon_box.textContent = this.render_map[i][j];
+                horizon_box.addEventListener("click", function() {
+                    game.render_box_open(this);
+                });
+                horizon_box.addEventListener("contextmenu", function() {
+                    game.render_flag(this);
+                });
                 vertical_box.appendChild(horizon_box);
+                horizon_html_render_list.push(horizon_box);
             }
             game_box.appendChild(vertical_box);
+            this.html_render_list.push(horizon_html_render_list);
         }
     }
+    test_render(){
+        for (let i = 0; i < this.html_render_list.length; i++) {
+            for (let j = 0; j < this.html_render_list[i].length; j++) {
+                this.html_render_list[i][j].textContent = this.mine_map[i][j];
+            }
+        }
+    }
+    render_box_open(elem) {
+        let coordinate = elem.id.split(",").map(Number);
+        if (this.first) {
+            this.set_mine_map(coordinate);
+            this.first = false;
+        }
+        this.test_render();
+        this.render_map[coordinate[0]][coordinate[1]] = -1
+        if ((this.mine_map[coordinate[0]][coordinate[1]]) === -1) {
+            console.log("Bomb!")
+        }
+        this.html_render_list[coordinate[0]][coordinate[1]].classList.remove("bg-green-200");
+        this.html_render_list[coordinate[0]][coordinate[1]].classList.add("bg-gray-400");
+    }
+    render_flag(elem) {
+        elem.classList.remove("bg-green-200");
+        elem.classList.add("bg-red-400");
+    }    
 }
-
-
-
-const selecter = document.getElementsByClassName("difficulty")
 for (let i = 0; i < selecter.length; i++) {
     selecter[i].addEventListener("click", function(){
-        const game = new mine_sweeper(difficulty_arry[this.id][0], difficulty_arry[this.id][1], difficulty_arry[this.id][2]);
-        game.create_map();
-        game.game_starter();
+        const game_instance = new mine_sweeper(difficulty_arry[this.id]);
+        game_instance.render_game_start(game_instance)
     });
 }
